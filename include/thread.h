aef4975cb07a7303647852ca59b951ac87e3e441
Patrick Escorsi <patrick.escorsi@ufms.br>
2021-06-04 15:36:42 -0300

    Comentários da aula

--- include/thread.h
+++ include/thread.h
@@ -19,8 +19,17 @@ typedef enum {
 
 typedef struct tcb {
 	// TODO: define the fields for you TCB
+	//precisa ter espaço para guardar um thread quando ela for retirada da CPU
+	uint64_t regs[NUMBER_OF_REGISTERS]; //espaço para registradores da CPU: rax, rbx, rcx, rdx, rbp, rsi
+										// rdi, r8, r9, r10, r11, r12, r13, r14, r15, rsp (ponteiro para pilha)
+	uint64_t rflags; //registrador de flags pushfq e popfq
+	// PRECISA ter uma pilha exclusiva para a thread STACK_SIZE 2048 	
+	// tid -> identificador da thread
+	//movq %rax, %rbx = %rbx = %rax
+	//pushq %rax = armazena o valor de rax no topo da pilha
+	//popq (reg ou mem) 
+	//leaq 
 	
-	status_t status;
 } tcb_t;
 
 #endif /* THREAD_H */
